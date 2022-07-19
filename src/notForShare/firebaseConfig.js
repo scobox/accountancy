cf7269ca138f20a8fabe3d11cfd2c3209c4949ea
Oleg Borisov <treex.lights@google.com>
2022-07-19 19:23:52 +0930

    added db config

---
+++ src/notForShare/firebaseConfig.js
@@ -0,0 +1,12 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+export const firebaseConfig = {
+	apiKey: "AIzaSyBJBDnNcD0-UBWEDnKslD90YsDdkkLl7K4",
+	authDomain: "accountant-fb189.firebaseapp.com",
+	databaseURL: "https://accountant-fb189-default-rtdb.asia-southeast1.firebasedatabase.app",
+	projectId: "accountant-fb189",
+	storageBucket: "accountant-fb189.appspot.com",
+	messagingSenderId: "91847248690",
+	appId: "1:91847248690:web:33a21727ff0c12a1c5652b"
+};
